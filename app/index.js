d62a7c29c29e131a5a4aac7ca4dea99684cfc624
Maxi Romero <mromerov@fi.uba.ar>
2022-05-06 20:18:44 -0300

    Add endpoints proxy

--- app/index.js
+++ app/index.js
@@ -2,8 +2,27 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const hostProxySync = "http://svc1:9090/";
+const hostProxyAsync = "http://svc2:9091/";
+
+const axios = require('axios');
+
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+	res.send('Hello World! \n');
+});
+
+app.get('/proxy-sync', (req, res) => {
+	axios.get(hostProxySync)
+		.then(result => {
+			res.send(`${result.data} \n`);
+		})
+});
+
+app.get('/proxy-async', (req, res) => {
+	axios.get(hostProxyAsync)
+		.then(result => {
+			res.send(`${result.data} \n`);
+		})
 });
 
 app.listen(port, () => {
