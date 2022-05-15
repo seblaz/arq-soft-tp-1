e5839764556a4ee9a52f4f17cbcf913a2c71cf8d
Maxi Romero <mromerov@fi.uba.ar>
2022-05-15 12:34:08 -0300

    Se agrega endpoint heavy

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 3000;
 
 const hostProxySvc1 = "http://svc1:9090/";
 const hostProxySvc2 = "http://svc2:9091/";
+const TIMEOUT = 10000;
 
 const axios = require('axios');
 
@@ -23,6 +24,13 @@ app.get('/proxy2', (req, res) => {
 		.then(result => {
 			res.send(`${result.data} \n`);
 		})
+});
+
+app.get('/heavy', (req, res) => {
+	let start = new Date();
+
+	while (new Date() - start < TIMEOUT);
+	res.send("Hello world! \n");
 });
 
 app.listen(port, () => {
